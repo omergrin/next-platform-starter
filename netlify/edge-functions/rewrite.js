ec4210ef1aa2df46ae9f3be57c910f8474d36901
Omer Grinboim <omer364@gmail.com>
2025-04-16 14:14:04 +0300

    test

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,3 +1,6 @@
+const x = require('hud-sdk');
+console.log(x);
+
 const rewrite = async (request, context) => {
     const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
     return new URL(path, request.url);
